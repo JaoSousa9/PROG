0e2fb642a705df2924c914b3dfe01fc5346448ff
JoaoSousa <joao.bernardo.s10@gmail.com>
2024-03-08 16:51:47 +0000

    small fix on exercise 4

--- tp4/hstr-to-integer.cpp
+++ tp4/hstr-to-integer.cpp
@@ -1,25 +1,24 @@
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
 unsigned long hstr_to_integer(const char hstr[]){
     unsigned long result = 0;
     int length = 0;
-    int power = 0;
+    unsigned long power = 1;
     for(int j = 0; hstr[j] != '\0'; j++){
         length++;
     }
 
     for(int i = length-1; i >= 0; i--){
         if(hstr[i]>= '0' && hstr[i] <= '9'){
-            result += (hstr[i]-48)*pow(16,power);
+            result += (hstr[i]-48)*power;
         }else if(hstr[i]>= 'a' && hstr[i] <= 'f'){
-            result += (hstr[i]-87)*pow(16,power);
+            result += (hstr[i]-87)*power;
         }else if(hstr[i]>= 'A' && hstr[i] <= 'F'){
-            result += (hstr[i]-55)*pow(16,power);
+            result += (hstr[i]-55)*power;
         }
-        power++;
+        power = power *  16;
     }
 
     return result;
@@ -27,11 +26,11 @@ unsigned long hstr_to_integer(const char hstr[]){
 
 int main(){
 
-    cout << hstr_to_integer("CafeBabe2022") << "\n";
+    cout << hstr_to_integer("cafebabe2022") << "\n";
     cout << hstr_to_integer("0") << "\n";
     cout << hstr_to_integer("A") << "\n";
     cout << hstr_to_integer("19") << "\n";
-    cout << hstr_to_integer("a");
+    cout << hstr_to_integer("fF");
 
     return 0;
 }
